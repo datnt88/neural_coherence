7b8dfc34f01d4a20d15cafa34df8e958b3eb920e
Dat Tien Nguyen <datnt88@gmail.com>
2017-01-06 00:35:25 +0300

    n-th commit

--- cnn_coherence_02.py
+++ cnn_coherence_02.py
@@ -1,11 +1,47 @@
-from keras.layers import Input, Embedding, LSTM, Dense, merge
+from keras.layers import Input, Embedding, LSTM, Dense, merge, Convolution1D, MaxPooling1D, Dropout
 from keras.models import Model
+
+import numpy as np
+import utilities 
 
 def ranking_loss(y_true, y_pred):
     pos = y_pred[:,0]
     neg = y_pred[:,1]
     loss = -K.sigmoid(pos-neg) # use loss = K.maximum(1.0 + neg - pos, 0.0) if you want to use margin ranking loss
-return K.mean(loss) + 0 * y_true
+    return K.mean(loss) + 0 * y_true
+
+
+
+
+#loading pos entity-gird 
+X_train_1, X_test_1, X_dev_1, E = utilities.load_and_numberize_data(path="./data/1/", dev_train_merge=0)
+
+#loading permuted entity-gird 
+X_train_0, X_test_0, X_dev_0, E = utilities.load_and_numberize_data(path="./data/0/", dev_train_merge=0)
+
+assert len(X_train_1) == len(X_train_0) or len(X_test_1) == len(X_test_0) or len(X_dev_1) == len(y_dev_0)
+
+num_train = len(X_train_1)
+num_dev = len(X_dev_1)
+num_test = len(X_test_1)
+
+#assign Y value
+y_train_1 = [1] * num_train 
+y_train_0 = [0] * num_train 
+
+y_dev_1 = [1] * num_dev 
+y_dev_0 = [0] * num_dev 
+
+y_test_1 = [1] * num_test 
+y_test_0 = [0] * num_test
+
+#randomly shuffle the training data
+np.random.seed(133)
+np.random.shuffle(X_train_1)
+np.random.seed(133)
+np.random.shuffle(X_train_0)
+
+print(y_train_0)
 
 
 #hyper parameres
@@ -15,7 +51,7 @@ pool_length = 4
 dropout_ratio = 0.5
 hidden_size = 250
 
-# first, define a CNN model for sequen of entities §
+# first, define a CNN model for sequen of entities 
 # input of sequences of X,O,S,-,P between 1 and 5
 sent_input = Input(shape=(100,), dtype='int32', name='sent_input')
 
