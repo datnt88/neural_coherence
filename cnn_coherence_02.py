ded010c962b6ac2d96210b7484d8a9add72e3ed5
Dat Tien Nguyen <datnt88@gmail.com>
2017-01-06 15:35:31 +0300

    very big update

--- cnn_coherence_02.py
+++ cnn_coherence_02.py
@@ -61,8 +61,14 @@ X_train_0 = sequence.pad_sequences(X_train_0, maxlen)
 X_dev_0   = sequence.pad_sequences(X_dev_0, maxlen)
 X_test_0   = sequence.pad_sequences(X_test_0, maxlen)
 
+
+# the output is always 1??????
 y_train_1 = np_utils.to_categorical(y_train_1, 2) 
-#y_train_0  = np_utils.to_categorical(y_test_0, 2)
+y_dev_1  = np_utils.to_categorical(y_dev_1, 2)
+y_dev_1[:,0] = 1
+y_train_1[:,0] = 1
+
+#print(y_dev_1)
 
 #randomly shuffle the training data
 np.random.seed(133)
@@ -70,8 +76,6 @@ np.random.shuffle(X_train_1)
 np.random.seed(133)
 np.random.shuffle(X_train_0)
 
-#loading embeddings
-E = data_helper.load_embeddings()
 
 #hyper parameres
 nb_filter = 150
@@ -80,12 +84,15 @@ pool_length = 4
 dropout_ratio = 0.5
 hidden_size = 250
 
+#loading embeddings
+E = data_helper.load_embeddings()
+
 # first, define a CNN model for sequence of entities 
 # input of sequences of X,O,S,-,P between 1 and 5
 sent_input = Input(shape=(500,), dtype='int32', name='sent_input')
 
 # embedding layer encodes the input into sequences of 300-dimenstional vectors. 
-x = Embedding(output_dim=300, input_dim=5, input_length=500)(sent_input)
+x = Embedding(output_dim=300, weights=[E], input_dim=5, input_length=500)(sent_input)
 
 # add a convolutiaon 1D layer
 x = Convolution1D(nb_filter=nb_filter, filter_length = filter_length, border_mode='valid', 
@@ -104,7 +111,6 @@ x = Dropout(dropout_ratio)(x)
 out_x = Dense(1, activation='linear')(x)
 shared_cnn = Model(sent_input, out_x)
 
-print(shared_cnn.summary())
 
 # Inputs of pos and neg document
 pos_input = Input(shape=(500,), dtype='int32')
@@ -114,24 +120,17 @@ neg_input = Input(shape=(500,), dtype='int32')
 pos_branch = shared_cnn(pos_input)
 neg_branch = shared_cnn(neg_input)
 
-# adding latent coherence score
-#pos_model = Dense(1, activation='linear')(pos_branch)
-#neg_model = Dense(1, activation='linear')(neg_branch)
-
 concatenated = merge([pos_branch, neg_branch], mode='concat',name="coherence_out")
 # output by two latent coherence score
-#predictions = Dense(2, activation='relu')(concatenated)
-
 
 final_model = Model([pos_input, neg_input], concatenated)
 
-final_model.compile(loss={'coherence_out': ranking_loss}, optimizer='rmsprop')
+final_model.compile(loss='ranking_loss', optimizer='adam')
 
-#final_model.compile(loss='ranking_loss', optimizer='rmsprop')
+#print(shared_cnn.summary())
+#print(final_model.summary())
 
-print(final_model.summary())
-
-final_model.fit([X_train_1, X_train_0], y_train_1, nb_epoch=10)
+final_model.fit([X_train_1, X_train_0], y_train_1, validation_data=([X_dev_1, X_dev_0], y_dev_1), nb_epoch=10)
 
 
 
