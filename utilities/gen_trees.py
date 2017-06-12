acf36d049eccbd0c525026f163977f44ae64ca3e
Dat T. Nguyen <datienguyen@wcw-self-145-109-125-236.wireless.uva.nl>
2017-06-12 17:46:52 +0200

    update new gen trees

--- utilities/gen_trees.py
+++ utilities/gen_trees.py
@@ -1,64 +1,60 @@
 from __future__ import absolute_import
 import glob, os, csv, re
-
+import sys
 
 def insert_node(x=[],node="100"):
 	tree_4 = []
+
+	#addin new branch
 	for item in x:
 		tmp = [k for k in item] 
 		tmp.append("1" + node)
 		tree_4.append(tmp)
+	
+	for item in x:
 		for i in item:
-			#adding 1-4
-			new_branch = i + node
-			#print "new branch: " + new_branch
+			#keep old_branches
 			old_branches = [branch for branch in item if branch != i]
-			#print "old branch: " + str(old_branches)
-			old_branches.append(new_branch)
-			tree_4.append(old_branches)
+			#compute new sub trees
+			new_subTrees = adding(x=i, new_node=node)
+			
+			for tree in new_subTrees:
+				tmp = [branch for branch in tree if branch not in old_branches]
+				#print tmp
+				# each new_braches, adding to the old branches 
+				tree_4.append(old_branches + tmp )
+
 	return tree_4
 
 
-def swap(x=[],n1="3",n2="4"):
+def adding(x="123",new_node="4"):
 	res = []
-	for tree in x:
-		new_tree = []
-		check = 0	
-		for branch in tree:
-			#find if there is n1, n2 in the branch
-			if find(branch,n1,n2) == True:
-				#swap then add
-				tmp = branch.replace(n1,"x")
-				tmp = tmp.replace(n2,n1)
-				tmp = tmp.replace("x",n2)
-				new_tree.append(tmp)
-				#print tmp
-				check = 1
-			else:
-				new_tree.append(branch)
-		if check == 1:
-			res.append(tree)
-		res.append(new_tree)
+	res.append([x + new_node])
+	n = len(x)
+
+	tmp = []
+	for i in range(1,n):
+		tmp = []
+		tmp.append(x)
+		tmp.append(x[0:i] + new_node)
+
+		res.append(tmp)
+
+
 	return res
 
-def find(s="",n1="3", n2="4"):
-	if s.find(n1) <0:
-		return False
-	if s.find(n2) < 0:
-		return False
-	return True
-	
-x = ['12', '13', '145']
-k = swap(x=x,n1="4",n2="5")
+#res = adding(x="123",new_node="4")
+#for i in res:
+#	print i
+#print "-----------------\n"
 
 
 def gen_Tree3():
-	return [["123"],["12","13"]]
+	return [["123"],["13","12"]]
 
 def gen_Tree4():
 	x3 = gen_Tree3() 
 	tree_4 = insert_node(x=x3,node="4")
-	tree_4.append(['123', '124'])
 	return tree_4
 
 def gen_Tree5():
@@ -66,23 +62,12 @@ def gen_Tree5():
 	x4 = gen_Tree4() 
 	tree_5 = insert_node(x=x4,node="5")
 
-	tree_5.append(['123', '124', '125'])
-	tree_5.append(['1234', '125'])
-	tree_5.append(['1234', '1235'])
- 	
-
 	#print ['1234','1235'] in tree_5
-
 	return tree_5
 
 def gen_Tree6():
 	x = gen_Tree5() 
 	tree_6 = insert_node(x=x,node="6")
-	tree_6.append(['123', '124', '125','126'])
-	tree_6.append(['1234', '1235','1236'])
-	tree_6.append(['12345', '126'])
-	tree_6.append(['12345', '1236'])
-	tree_6.append(['12345', '12346'])
 	
 	#print ['1246', '1245','1234'] in tree_6
 
@@ -101,12 +86,38 @@ def gen_tree_branches(n=3):
 		print "Have not supported larger threads (num of commnet > 6) yet...!"
 		return None
 
-x = gen_tree_branches(5)
+def remove_duplication(x=[]):
+	sort_x = []
+	for i in x:
+		sort_x.append('.'.join(sorted(i)))
+	
+	uniq_x = list(set(sort_x))
 
-for i in x:
+	res = []
+	for i in uniq_x:
+		res.append(i.split("."))
+
+	return res
+
+
+'''
+x = gen_tree_branches(int(sys.argv[1]))
+#for i in x:
+#	print i
+#print "-------------------------------"
+#print len(x)
+
+print "-------------------------------"
+
+final = remove_duplication(x=x)
+for i in final:
 	print i
-print "-----------------\n"
-print len(x)
+print "-------------------------------"
+print len(final)
+'''
+
+
+
 
 	
 
