0afdbc49ef820f23637276119cfd911bf87c2b42
Dat T. Nguyen <datienguyen@wcw-self-145-109-127-170.wireless.uva.nl>
2017-07-17 14:32:51 +0200

    to be perfect

--- wsdm17_edge_level.py
+++ wsdm17_edge_level.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         ,minibatch_size = 32
         ,dropout_ratio  = 0.5
 
-        ,maxlen         = 14000
+        ,maxlen         = 10000
         ,epochs         = 30
         ,emb_size       = 100
         ,hidden_size    = 250
